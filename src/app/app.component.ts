953e595efa6b21d39f20ee19223fb7dad24c7462
BIRD-MAC <chalermporn@aspiron.co.th>
2019-02-24 21:16:17 +0700

    Create Todo App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { setTheme } from "ngx-bootstrap/utils";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = "todo-app";
+
+  newTodo: string;
+  todos: any;
+  todoObj: any;
+
+  constructor() {
+    setTheme("bs4");
+    this.newTodo = "";
+    this.todos = [];
+  }
+
+  addTodo(event: any) {
+    this.todoObj = {
+      newTodo: this.newTodo,
+      completed: false
+    };
+    this.todos.push(this.todoObj);
+    this.newTodo = "";
+    event.preventDefault();
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
